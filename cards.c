dedae9c9bc43ae1f4f0cf29be009f9328054ed70
Jasper Sival <jaspersival@gmail.com>
2022-07-09 13:32:22 +0200

    Card game with do-while loop

--- cards.c
+++ cards.c
@@ -4,25 +4,41 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch (card_name[0])
+    int count = 0;
+    do
     {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case 'A':
-        val = 11;
-        break;
-    default:
-        val = atoi(card_name);
-    }
-    if ((val >= 3) && (val <= 6))
-        puts("Count has gone up");
-    else if (val == 10)
-        puts("Cound has gone down");
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch (card_name[0])
+        {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            continue;
+        default:
+            val = atoi(card_name);
+            if ((val < 1) || (val > 10))
+            {
+                printf("Invalid value %i\n", val);
+                continue;
+            }
+        }
+        if ((val >= 3) && (val <= 6))
+        {
+            count++;
+        }
+        else if (val == 10)
+        {
+            count--;
+        }
+        printf("Current count: %i\n", count);
+    } while (card_name[0] != 'X');
     return 0;
 }
